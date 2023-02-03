c40a225367a0950e4d273601e33eb6ffce74e9a4
Yaniv Ben Hemo <Yaniv@memphis.dev>
2023-02-03 11:30:09 +0100

    delivery microservice added

--- delivery/app_part3_delivery.py
+++ delivery/app_part3_delivery.py
@@ -7,7 +7,7 @@ from memphis import Memphis, MemphisError, MemphisConnectError, MemphisHeaderErr
 load_dotenv()
 
 async def main():
-    async def insert_event_to_db(msgs, error, context):
+    async def deliver_an_order(msgs, error, context):
         try:
             for msg in msgs:
                 order = json.loads(msg.get_data())
@@ -23,7 +23,7 @@ async def main():
         await memphis.connect(host=os.getenv("MEMPHIS_URL"), username=os.getenv("MEMPHIS_USERNAME"), connection_token=os.getenv("MEMPHIS_CONNECTION_TOKEN"))
         
         consumer = await memphis.consumer(station_name=os.getenv("MEMPHIS_ORDERS_STATION_NAME"), consumer_name="database-worker1", consumer_group="database-workers")
-        consumer.consume(insert_event_to_db)
+        consumer.consume(deliver_an_order)
 
         # Keep your main thread alive so the consumer will keep receiving data
         await asyncio.Event().wait()
--- frontend/app_part2_memphis.py
+++ frontend/app_part2_memphis.py
@@ -3,22 +3,27 @@ from flask import Flask
 from flask import render_template
 from dotenv import load_dotenv
 import pymongo
+import secrets
 import datetime
 from memphis import Memphis, MemphisError, MemphisConnectError, MemphisHeaderError, MemphisSchemaError
 
 
 app = Flask(__name__)
 load_dotenv()
-myclient = pymongo.MongoClient("mongodb://"+ os.getenv('DATABASE_URL') +"/")
-mydb = myclient["resturant"]
-mycol = mydb["orders"]
+try:
+    myclient = pymongo.MongoClient("mongodb://"+ os.getenv('DATABASE_URL') +"/")
+    mydb = myclient["resturant"]
+    mycol = mydb["orders"]
+    print("Connected to DB")
+except:
+    print("No hosts found")
 
 async def produce_event_to_memphis(item):
     try:
         memphis = Memphis()
         await memphis.connect(host=os.getenv("MEMPHIS_URL"), username=os.getenv("MEMPHIS_USERNAME"), connection_token=os.getenv("MEMPHIS_CONNECTION_TOKEN"))
         producer = await memphis.producer(station_name=os.getenv("MEMPHIS_ORDERS_STATION_NAME"), producer_name="website", generate_random_suffix=True)
-        await producer.produce(item)
+        await producer.produce(message=item)
 
     except (MemphisError, MemphisConnectError, MemphisHeaderError, MemphisSchemaError) as e:
         print(e)
@@ -42,11 +47,19 @@ async def order(item={'name':'hamburger', 'quantity':1, 'status': "new_order"}):
     print(item)
     current_time = datetime.datetime.now()
     item["order_date"] = current_time.strftime("%m/%d/%Y, %H:%M:%S")
-    mycol.insert_one(item)
+    item["_id"] = secrets.token_hex(nbytes=12)
+    print("Saving the order to the DB")
+    try:
+        mycol.insert_one(item)
+    except:
+        print("DB is not available")
 
     # Push an order to memphis
-
-    await produce_event_to_memphis(item)
+    print("Pushing order to processing")
+    try:
+        await produce_event_to_memphis(item)
+    except:
+        print("Something wrong with the queue")
 
     return item
 
